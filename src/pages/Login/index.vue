50e67833554fc17bbd6c5c051206f9a651bb8780
马大哈 <liling@puluotech.com>
2023-07-14 00:36:15 +0800

    导入uView

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -23,6 +23,30 @@
       style="width: 142rpx; height: 34rpx; display: block; margin-top: 18rpx"
     />
 
+    <u-form labelPosition="left" :model="form" :rules="rules" ref="form">
+      <u-form-item labelWidth="80" label="手机号" prop="phone" borderBottom ref="item1">
+        <u-input v-model="form.phone" maxlength="11" placeholder="请填写手机号" border="none" />
+      </u-form-item>
+      <u-form-item label="验证码" prop="verificationCode" borderBottom labelWidth="80" ref="item1">
+        <u-input v-model="form.verificationCode" placeholder="请填写验证码" border="none">
+          <template slot="suffix">
+            <u-button
+              @click="getCode"
+              :text="tips"
+              type="success"
+              size="mini"
+              :disabled="disabled"
+              :hairline="false"
+              :customStyle="{
+                background: '#5ac725 !important',
+                padding: '0 8px !important',
+              }"
+            />
+          </template>
+        </u-input>
+      </u-form-item>
+    </u-form>
+
     <image
       src="/static/login/login_button.png"
       style="width: 426rpx; height: 94rpx; display: block; margin-top: 169rpx"
@@ -30,7 +54,40 @@
   </view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import ref from 'vue';
+
+  const form = ref();
+  const disabled = ref(true);
+  const tips = ref(true);
+
+  const getCode = () => {};
+
+  const rules = {
+    phone: [
+      {
+        required: true,
+        message: '请输入手机号',
+        trigger: ['change', 'blur'],
+      },
+      {
+        validator: (_rule: any, value: any, _callback: any) => {
+          const isOk = uni.$u.test.mobile(value);
+          return isOk;
+        },
+        message: '手机号码不正确',
+        trigger: ['change', 'blur'],
+      },
+    ],
+    verificationCode: {
+      type: 'string',
+      required: true,
+      len: 6,
+      message: '请填写6位验证码',
+      trigger: ['change', 'blur'],
+    },
+  };
+</script>
 
 <style lang="scss" scoped>
   .page {
